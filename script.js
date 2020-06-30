5f5145ce95c17e20fa06c7a228baac5284ced37c
David Keller <mrcodeswildride@gmail.com>
2020-06-30 17:58:47 +0000

    simplified layout

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 let box = document.getElementById(`box`)
 
+let colorParagraph = document.getElementById(`colorParagraph`)
 let redSlider = document.getElementById(`redSlider`)
 let greenSlider = document.getElementById(`greenSlider`)
 let blueSlider = document.getElementById(`blueSlider`)
-let colorParagraph = document.getElementById(`colorParagraph`)
 
 let randomButton = document.getElementById(`randomButton`)
 
-redSlider.addEventListener(`input`, changeColor)
-greenSlider.addEventListener(`input`, changeColor)
-blueSlider.addEventListener(`input`, changeColor)
+redSlider.addEventListener(`input`, mixColors)
+greenSlider.addEventListener(`input`, mixColors)
+blueSlider.addEventListener(`input`, mixColors)
 
-randomButton.addEventListener(`click`, changeRandomColor)
+randomButton.addEventListener(`click`, randomColor)
 
-function changeColor() {
-  box.style.backgroundColor = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`
-  colorParagraph.innerHTML = box.style.backgroundColor
+function mixColors() {
+  colorParagraph.innerHTML = `rgb(${redSlider.value}, ${greenSlider.value}, ${blueSlider.value})`
+  box.style.backgroundColor = colorParagraph.innerHTML
 }
 
-function changeRandomColor() {
+function randomColor() {
   redSlider.value = Math.floor(Math.random() * 256)
   greenSlider.value = Math.floor(Math.random() * 256)
   blueSlider.value = Math.floor(Math.random() * 256)
-  changeColor()
+  mixColors()
 }
